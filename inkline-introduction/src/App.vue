1b87a1554d19cd0df11ef3965e3b66597702edbe
NIKO <nikothewolfox@gmail.com>
2022-11-23 13:54:35 +0900

    コンポーネントを追加してページ風にする

--- inkline-introduction/src/App.vue
+++ inkline-introduction/src/App.vue
@@ -1,27 +1,76 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const inputValue = ref("")
 </script>
 
 <template>
   <i-layout>
     <i-layout-header>
-      Header
+      <i-navbar>
+        <i-navbar-brand>
+          Inkline Introduction
+        </i-navbar-brand>
+      </i-navbar>
     </i-layout-header>
-    <i-layout vertical>
+    <i-layout vertical class="_padding-top:1/2">
       <i-layout-aside>
-        Left Aside
+        <i-sidebar>
+          <i-nav vertical>
+            <i-nav-item>item1</i-nav-item>
+            <i-nav-item>item2</i-nav-item>
+            <i-nav-item>item3</i-nav-item>
+          </i-nav>
+        </i-sidebar>
       </i-layout-aside>
       <i-layout-content>
-        Content
+        <i-container fluid>
+          <i-row>
+            <i-column>
+              <h1>Some of Inkline Controls</h1>
+            </i-column>
+          </i-row>
+          <i-row>
+            <i-column>
+              <h2>Button</h2>
+              <i-button>Button</i-button>
+              <i-button color="primary">Primary</i-button>
+              <i-button color="secondary">Secondary</i-button>
+              <i-button color="success">Success</i-button>
+              <i-button color="danger">Danger</i-button>
+              <i-button color="warning">Warning</i-button>
+              <i-button color="info">Info</i-button>
+              <i-button color="light">Light</i-button>
+              <i-button color="dark">Dark</i-button>
+            </i-column>
+          </i-row>
+          <i-row>
+            <i-column>
+              <h2>Input</h2>
+              <i-input v-model="inputValue" placeholder="Type something.." />
+            </i-column>
+          </i-row>
+          <i-row>
+            <i-column>
+              <h2>Text</h2>
+              <p class="_text-align:left">
+                This is a left aligned text.
+              </p>
+              <p class="_text-align:center">This is a center aligned text.</p>
+              <p class="_text-align:right">This is a right aligned text.</p>
+            </i-column>
+          </i-row>
+        </i-container>
       </i-layout-content>
     </i-layout>
     <i-layout-footer>
-      Footer
+      <div class="_border">(c) 2022 NIKO</div>
     </i-layout-footer>
   </i-layout>
 </template>
 
 <style>
-.layout-aside {
-  width: 12rem;
+._border {
+  padding: 6px;
 }
 </style>
